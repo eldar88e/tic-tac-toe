d065adc381bbde6cf1006830bd5eb658c0ec727c
Eldar Eminov <em88el@gmail.com>
2026-04-20 02:44:11 +0300

    Refactor

--- src/ui/playersUi.js
+++ src/ui/playersUi.js
@@ -2,6 +2,9 @@ import { PixiElement } from '../utils/PixiElement.js';
 import { allTextureKeys } from '../common/assets.js';
 import { elementType, labels } from '../common/enums.js';
 
+const PLAYER_UI_HALF_WIDTH = 56;
+const PLAYER_UI_SIDE_PADDING = 24;
+
 export default function createPlayers(app) {
 	const playersContainer = new PixiElement({
 		type: elementType.CONTAINER,
@@ -20,7 +23,7 @@ export default function createPlayers(app) {
 		texture: allTextureKeys.cross,
 		scale: [0.11, 0.11],
 		anchor: [0.5, 0.5],
-		position: [28, 18],
+		position: [0, 18],
 	});
 
 	const playerOneText = new PixiElement({
@@ -33,7 +36,7 @@ export default function createPlayers(app) {
 			fontWeight: '700',
 		},
 		anchor: [0.5, 0],
-		position: [28, 44],
+		position: [0, 44],
 	});
 
 	const playerTwo = new PixiElement({
@@ -47,7 +50,7 @@ export default function createPlayers(app) {
 		texture: allTextureKeys.zero,
 		scale: [0.11, 0.11],
 		anchor: [0.5, 0.5],
-		position: [28, 18],
+		position: [0, 18],
 	});
 
 	const playerTwoText = new PixiElement({
@@ -60,24 +63,27 @@ export default function createPlayers(app) {
 			fontWeight: '700',
 		},
 		anchor: [0.5, 0],
-		position: [28, 44],
+		position: [0, 44],
 	});
 
 	playerOne.addChildren([playerOneIcon.getElement(), playerOneText.getElement()]);
 	playerTwo.addChildren([playerTwoIcon.getElement(), playerTwoText.getElement()]);
 
-	playerOneElement.position.set(playerOneElement.width, 0);
-	playerOneElement.pivot.set(playerOneElement.width / 2, playerOneElement.height / 2);
-
-	playerTwoElement.position.set(app.screen.width - playerTwoElement.width, 0);
-	playerTwoElement.pivot.set(playerTwoElement.width / 2, playerTwoElement.height / 2);
+	playerOneElement.position.set(PLAYER_UI_SIDE_PADDING + PLAYER_UI_HALF_WIDTH, 0);
+	playerTwoElement.position.set(
+		app.screen.width - PLAYER_UI_SIDE_PADDING - PLAYER_UI_HALF_WIDTH,
+		0
+	);
 
 	playersContainer.addChildren([playerOneElement, playerTwoElement]);
 	playersContainerElement.position.set(0, playersContainerElement.height / 2 + 25);
 
 	function onResizeHandler() {
-		playerOneElement.position.set(playerOneElement.width, 0);
-		playerTwoElement.position.set(app.screen.width - playerTwoElement.width, 0);
+		playerOneElement.position.set(PLAYER_UI_SIDE_PADDING + PLAYER_UI_HALF_WIDTH, 0);
+		playerTwoElement.position.set(
+			app.screen.width - PLAYER_UI_SIDE_PADDING - PLAYER_UI_HALF_WIDTH,
+			0
+		);
 	}
 
 	return playersContainerElement;
